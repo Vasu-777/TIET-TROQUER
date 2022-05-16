ab3e2722ec1606d3329e158b01c5548c76903c8a
Vasu-777 <vasunagori777@gmail.com>
2022-05-16 23:58:11 +0530

    Add Project

---
+++ coupons/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('available/',					views.available,		name='available'),
+    path('available/<int:id>',			views.available,		name='available'),
+    path('purchased/',			views.purchased,		name='purchased'),
+    path('issued/<int:pk>',			views.issued_coupons,		name='issued'),
+    # path('issued/<int:pk>',			views.issued_coupons,		name='issued'),
+    # path('test1/',            views.test1,     name='test1'),
+]
